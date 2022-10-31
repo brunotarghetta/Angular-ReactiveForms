6e6a4c485f928a2853f1b63dab073971dd74a76c
brunotarghetta <brunotarghetta@gmail.com>
2022-10-31 16:30:35 -0300

    Building a Reactive Form

--- demo-start-local/src/app/customers/customer.component.ts
+++ demo-start-local/src/app/customers/customer.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-
+import {FormGroup, FormBuilder} from '@angular/forms';
 import { Customer } from './customer';
+
 
 @Component({
   selector: 'app-customer',
@@ -9,15 +9,45 @@ import { Customer } from './customer';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
+  
+  customerForm!: FormGroup;
   customer = new Customer();
 
-  constructor() { }
+  constructor( private fb: FormBuilder) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.customerForm = this.fb.group({
+      firstName: '',
+      lastName: {value: 'n/a', disabled: true},
+      email: '',
+      sendCatalog: true
+    })
+    
+    // this.customerForm = new FormGroup({
+    //   firstName: new FormControl(),
+    //   lastName: new FormControl(),
+    //   email: new FormControl(),
+    //   sendCatalog: new FormControl(true)
+    // });
   }
 
-  save(customerForm: NgForm): void {
-    console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  populateTestData(): void{
+    this.customerForm.setValue({
+      firstName: 'Jack',
+      lastName: 'Harkess',
+      email: 'jack@torchwood.com',
+      sendCatalog: false
+    }); 
+
+    // this.customerForm.patchValue({
+    //   firstName: 'Jack',
+    //   lastName: 'Harkess',      
+    //   sendCatalog: false
+    // }); 
+  }
+
+  save(): void {
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm.value));
   }
 }
